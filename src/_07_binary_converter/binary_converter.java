744ae574057bdee37831551ed1fd6d26a9246f7a
OceanNut420 <54760319+OceanNut420@users.noreply.github.com>
2021-08-20 18:30:31 -0700

    Update binary_converter.java
    fan pi

--- src/_07_binary_converter/binary_converter.java
+++ src/_07_binary_converter/binary_converter.java
@@ -1,9 +1,20 @@
 package _07_binary_converter;
 
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 public class binary_converter {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		JPanel panel = new JPanel();
+		JFrame frame = new JFrame();
+		JTextField text = new JTextField(20);
+		
+		
+
 
 	}
 
