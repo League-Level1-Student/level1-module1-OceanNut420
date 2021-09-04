b39f5b6dbe855e5c2228f89169ad398bafa61df7
OceanNut420 <54760319+OceanNut420@users.noreply.github.com>
2021-09-03 18:23:45 -0700

    fanpi+
    eeee

--- src/_07_binary_converter/binary_converter.java
+++ src/_07_binary_converter/binary_converter.java
@@ -13,7 +13,26 @@ public class binary_converter {
 		JFrame frame = new JFrame();
 		JTextField text = new JTextField(20);
 		
-		
+		String convert(String input) {
+		    if(input.length() != 8){
+		        JOptionPane.showMessageDialog(null, "Enter 8 bits, silly!!!");
+		        return "-";
+		    }
+		    String binary = "[0-1]+";//must contain numbers in the given range
+		    if (!input.matches(binary)) {
+		        JOptionPane.showMessageDialog(null, "Binary can only contain 1s or 0s, silly!!!");
+		        return "-";   
+		    }
+		    try {
+		        int asciiValue = Integer.parseInt(input, 2);
+		        char theLetter = (char) asciiValue;
+		        return "" + theLetter;
+		    } catch (Exception e) {
+		        JOptionPane.showMessageDialog(null, "Enter a binary, silly!!!");
+		        return "-";
+		    }
+		}
+
 
 
 	}
